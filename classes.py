5cc9e6ebf63549632da6fea576ddd9fb260ae6e4
dreamxinxcode <dreamxinxcode@gmail.com>
2020-08-30 10:50:41 -0700

    first commit

---
+++ classes.py
@@ -0,0 +1,28 @@
+import  os
+
+class Car():
+    def __init__(self, make, model, year, current_speed, top_speed):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.current_speel = current_speed
+        self.top_speed = top_speed
+
+        def accellarate(self):
+            for i in range(0, top_speed+1):
+                print(current_speed)
+                current_sped += 1
+
+        def brake(self):
+            for i in range(0, current_speed +1):
+                current_speed -= 1 
+                print(current_speed)
+
+
+my_car = Car('VW', 'Golf', '2005', 0, 220)
+
+my_car.accellarate()
+
+            
+
+
